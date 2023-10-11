f90063a7a0358ad67ebc448d88f63a9d5761fbd0
Dell-User <Dell-User@DESKTOP-78SJPVI>
2023-10-11 09:20:44 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17.Classes_and_Objects
+{
+    internal class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+    }
+   internal class program
+    { 
+        static void Main(string[] args)
+        {
+            Book book1 = new Book(); 
+
+            
+            book1.title = "harry porter";
+            book1.author = "JK Rawling";
+            book1.pages = 400;
+
+            Book book2 = new Book();
+            book2.title = "lord of the rings";
+            book2.author = "Tolkein";
+            book2.pages = 900;
+
+            Console.WriteLine(book1.pages);
+            Console.WriteLine(book2.title);
+            
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
